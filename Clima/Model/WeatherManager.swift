1275ef3749c927c68da3cb046e2608f9df55f3f8
Daniil Alekseev <dapcula08@yandex.ru>
2020-06-27 14:09:11 +0300

    Networking Complete

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Даниил Алексеев on 26.06.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=344bae6a2182cad47c35c815e501c324&units=metric"
+    
+    func fetchWeather(cityName: String ){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+        let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
+}
